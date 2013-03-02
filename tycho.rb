714a66db14ce7f525ef71086a13d0aba250efb52
Jed Foster <jed@jedfoster.com>
2013-03-01 18:15:39 -0800

    Basic subdomain support.

--- tycho.rb
+++ tycho.rb
@@ -10,6 +10,8 @@ require 'sinatra/partial'
 require 'json'
 require 'github_api'
 
+require 'rack/request'
+
 require 'sass'
 require 'compass'
 require 'yaml'
@@ -17,6 +19,21 @@ require 'github/markdown'
 
 require 'redcarpet'
 require 'RedCloth'
+
+
+module Rack
+  class Request
+    def subdomains(tld_len=1) # we set tld_len to 1, use 2 for co.uk or similar
+      # cache the result so we only compute it once.
+      @env['rack.env.subdomains'] ||= lambda {
+        # check if the current host is an IP address, if so return an empty array
+        return [] if (host.nil? ||
+                      /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(host))
+        host.split('.')[0...(1 - tld_len - 2)] # pull everything except the TLD
+      }.call
+    end
+  end
+end
 
 
 set :partial_template_engine, :erb
