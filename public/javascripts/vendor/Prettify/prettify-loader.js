2a2ae2e104716df2e5a84f6b4a9603151ad78e1f
Jed Foster <jed@jedfoster.com>
2013-03-12 08:42:08 -0700

    Rewrite lots of the home page, tweak styling of code tags.
    Also add .no-prettify class to turn off Prettification of select elements.

--- public/javascripts/vendor/Prettify/prettify-loader.js
+++ public/javascripts/vendor/Prettify/prettify-loader.js
@@ -2,8 +2,8 @@ pretty = function() {
   // Add pretty print to all pre and code tags.
   $('pre, code').addClass("prettyprint");
 
-  // Remove prettify from code tags inside pre tags.
-  $('pre code').removeClass("prettyprint");
+  // Remove prettify from code tags inside pre tags and from code or pre tags with class .no-prettify.
+  $('pre code, .no-prettify').removeClass("prettyprint");
 
   $('.prettyprint').addClass(function () {
     return 'lang-' + $(this).attr('lang');
