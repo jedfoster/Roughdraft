3d66def173b6493849e73f8335adf4c0740074ac
Jed Foster <jed@jedfoster.com>
2013-03-12 10:29:20 -0700

    Move bookmarklet link to right sidebar at desktop breakpoint.
    Also, make it pretty.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,13 @@
-// (function($) {
-//   
-// })(jQuery);
+(function($) {
+  function positionBookmarklet() {
+    if($('#bookmarklet').css("position") == 'absolute') {
+      var offset = $('#bookmarklet_steps').position().top;
+      console.log($('#bookmarklet_steps').position().top);
+      $('#bookmarklet').offset({top: offset});
+    }
+  }
+  
+  positionBookmarklet();
+  
+  $(window).resize(positionBookmarklet);
+})(jQuery);
