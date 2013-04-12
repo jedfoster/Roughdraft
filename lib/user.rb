064b2a9974ce2934d1f1406efc54044b0b86ef3d
Jed Foster <jed@jedfoster.com>
2013-04-12 13:23:07 -0700

    Return false from User.new if GitHub returns NotFound

--- lib/user.rb
+++ lib/user.rb
@@ -28,9 +28,13 @@ class User < Hash
 
 private
   def fetch(id)
-    user = Github::Users.new.get(user: id, client_id: Roughdraft.gh_config['client_id'], client_secret: Roughdraft.gh_config['client_secret'])
+    begin
+      user = Github::Users.new.get(user: id, client_id: Roughdraft.gh_config['client_id'], client_secret: Roughdraft.gh_config['client_secret'])
 
-    REDIS.setex(user['login'], 60, user.to_hash.to_json)
-    user.to_hash
+      REDIS.setex(user['login'], 60, user.to_hash.to_json)
+      user.to_hash
+    rescue Github::Error::NotFound
+      false
+    end
   end
 end
