2fd98c1329c5768ca20d4631c9edd6f0632a56ea
Jed Foster <jed@jedfoster.com>
2013-07-12 12:57:49 -0700

    Do not try to load comments on edit or new views

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -1,39 +1,40 @@
 (function($) {
-  $.ajax({
-    dataType: "json",
-    url: window.location.pathname + '/comments.json',
-    success: function(data) {
+  if( ! window.location.pathname.match(/\/(new|edit)/) ) {
+    $.ajax({
+      dataType: "json",
+      url: window.location.pathname + '/comments.json',
+      success: function(data) {
 
-      var author = window.location.host.split('.').shift();
+        var author = window.location.host.split('.').shift();
 
-      var comments = document.createElement('ul');
-      comments.style.display = 'none';
+        var comments = document.createElement('ul');
+        comments.style.display = 'none';
 
-      $(data).each(function() {
-        $(comments).append('<li class="comment' + ((author == this.user.login) ? ' author-comment' : '' ) + '">\
-          <h3>\
-            <a href="https://github.com/' + this.user.login + '" class="login">' + this.user.login + '</a>\
-            <time datetime="' + this.created_at + '">' + this.created_at_formatted + '</time>\
-          </h3>\
-          <div class="body">\
-            ' + this.body_rendered + '\
-          </div>\
-        </li>');
-      });
+        $(data).each(function() {
+          $(comments).append('<li class="comment' + ((author == this.user.login) ? ' author-comment' : '' ) + '">\
+            <h3>\
+              <a href="https://github.com/' + this.user.login + '" class="login">' + this.user.login + '</a>\
+              <time datetime="' + this.created_at + '">' + this.created_at_formatted + '</time>\
+            </h3>\
+            <div class="body">\
+              ' + this.body_rendered + '\
+            </div>\
+          </li>');
+        });
 
-      $('.content').append('<footer id="comments"><h2 id="toggle-comments">Comments</h2></footer>');
+        $('.content').append('<footer id="comments"><h2 id="toggle-comments">Comments</h2></footer>');
 
-      $('#comments').append(comments);
+        $('#comments').append(comments);
 
-      $('#toggle-comments').on('click', function(event) {
-        $('#comments ul').toggle();
-      });
+        $('#toggle-comments').on('click', function(event) {
+          $('#comments ul').toggle();
+        });
 
-      pretty();
-    },
-    error: function(data) {
-      console.log('ERROR!!!!');
-      console.log(data);
-    }
-  });
+        pretty();
+      },
+      error: function(data) {
+        // Do nothing
+      }
+    });
+  }
 })(jQuery);
