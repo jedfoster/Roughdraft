094d28582cdeeaf1a1ad56bb9a6688115435e9df
Jed Foster <jed@jedfoster.com>
2013-07-28 07:16:35 -0700

    Move Rack::Request code to lib file

--- roughdraft.rb
+++ roughdraft.rb
@@ -12,6 +12,7 @@ require 'html/pipeline'
 require 'RedCloth'
 require 'haml'
 
+require './lib/rack/request.rb'
 require './lib/gist.rb'
 require './lib/user.rb'
 require './lib/gist_list.rb'
@@ -26,19 +27,6 @@ Sinatra::Application.register Sinatra::RespondTo
 
 HTML::Pipeline::SanitizationFilter::WHITELIST[:attributes][:all].push 'class'
 
-module Rack
-  class Request
-    def subdomains(tld_len=1) # we set tld_len to 1, use 2 for co.uk or similar
-      # cache the result so we only compute it once.
-      @env['rack.env.subdomains'] ||= lambda {
-        # check if the current host is an IP address, if so return an empty array
-        return [] if (host.nil? ||
-                      /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(host))
-        host.split('.')[0...(1 - tld_len - 2)] # pull everything except the TLD
-      }.call
-    end
-  end
-end
 
 
 
