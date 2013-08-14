71949fe6c859cce4f1331aa4266f470683fa7091
Jed Foster <jed@jedfoster.com>
2013-08-13 21:28:49 -0700

    Clean up

--- roughdraft.rb
+++ roughdraft.rb
@@ -2,11 +2,11 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/partial'
+require 'sinatra/respond_to'
+require 'chairman'
 require 'json'
 require 'github_api'
 require 'rack/request'
-# require 'sass'
-# require 'compass'
 require 'yaml'
 require 'html/pipeline'
 require 'RedCloth'
@@ -22,10 +22,6 @@ require './lib/html/pipeline/haml.rb'
 require './lib/html/pipeline/gist.rb'
 require './lib/roughdraft.rb'
 
-require 'chairman'
-
-require 'sinatra/respond_to'
-
 
 class RoughdraftApp < Sinatra::Base
   register Sinatra::RespondTo
@@ -33,261 +29,246 @@ class RoughdraftApp < Sinatra::Base
 
   HTML::Pipeline::SanitizationFilter::WHITELIST[:attributes][:all].push 'class'
 
+  set :partial_template_engine, :erb
 
-set :partial_template_engine, :erb
+  set(:subdomain) { |num_subdomains| condition { request.subdomains.count == num_subdomains } }
 
-set(:subdomain) { |num_subdomains| condition { request.subdomains.count == num_subdomains } }
 
-# ENV["DEBUG"] = "true"
+  configure do
+    require 'redis'
+    redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+    uri = URI.parse(redisUri)
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
 
-configure do
-  require 'redis'
-  redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
-  uri = URI.parse(redisUri)
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+  configure :production do
+    APP_DOMAIN = 'roughdraft.io'
 
-configure :production do
-  APP_DOMAIN = 'roughdraft.io'
+    helpers do
+      use Rack::Session::Cookie, :key => 'roughdraft.io',
+                                 :domain => '.roughdraft.io',
+                                 :path => '/',
+                                 :expire_after => 7776000, # 90 days, in seconds
+                                 :secret => ENV['COOKIE_SECRET']
+    end
+
+    Chairman.new(ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], ['gist', 'user'])
+  end
+
+  configure :development do
+    APP_DOMAIN = 'roughdraft.dev'
+
+    helpers do
+      use Rack::Session::Cookie, :key => 'roughdraft.dev',
+                                 # :domain => :all,
+                                 :domain => '.roughdraft.dev',
+                                 :path => '/',
+                                 :expire_after => 7776000, # 90 days, in seconds
+                                 :secret => 'local'
+    end
+
+    yml = YAML.load_file("config/github.yml")
+    Chairman.config(yml["client_id"], yml["client_secret"], ['gist', 'user'])
+  end
+
 
   helpers do
-    use Rack::Session::Cookie, :key => 'roughdraft.io',
-                               :domain => '.roughdraft.io',
-                               :path => '/',
-                               :expire_after => 7776000, # 90 days, in seconds
-                               :secret => ENV['COOKIE_SECRET']
-   end
+    include ERB::Util
+    alias_method :code, :html_escape
 
-  Chairman.new(ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], ['gist', 'user'])
-end
-
-configure :development do
-  APP_DOMAIN = 'roughdraft.dev'
-
-  helpers do
-    use Rack::Session::Cookie, :key => 'roughdraft.dev',
-                               # :domain => :all,
-                               :domain => '.roughdraft.dev',
-                               :path => '/',
-                               :expire_after => 7776000, # 90 days, in seconds
-                               :secret => 'local'
+    include Roughdraft
   end
 
-  yml = YAML.load_file("config/github.yml")
-  Chairman.config(yml["client_id"], yml["client_secret"], ['gist', 'user'])
-end
 
-
-helpers do
-  include ERB::Util
-  alias_method :code, :html_escape
-
-  include Roughdraft
-end
-
-
-before do
-  @user = @gist = @action = false
-  @github = Chairman.session(session[:github_token])
-end
-
-before :subdomain => 1 do
-  if request.subdomains[0] != 'www'
-    @user = User.new(request.subdomains[0])
+  before do
+    @user = @gist = @action = false
+    @github = Chairman.session(session[:github_token])
   end
-end
 
-
-get %r{^(/|/feed)$}, :provides => ['html', 'json', 'xml'] do
-  @action = 'home'
-
-  if @user
-    status, headers, body = call env.merge("PATH_INFO" => '/page/1')
-    [status, headers, body]
-  else
-    erb :index
+  before :subdomain => 1 do
+    if request.subdomains[0] != 'www'
+      @user = User.new(request.subdomains[0])
+    end
   end
-end
 
 
-get '/page/:page' do
-  @action = 'list'
+  get %r{^(/|/feed)$}, :provides => ['html', 'json', 'xml'] do
+    @action = 'home'
 
-  if @user
-    gists = GistList.new(@user.id, params[:page])
+    if @user
+      status, headers, body = call env.merge("PATH_INFO" => '/page/1')
+      [status, headers, body]
+    else
+      erb :index
+    end
+  end
 
-    headers 'X-Cache-Hit' => gists.from_redis
 
-    if gists.list.empty?
+  get '/page/:page' do
+    @action = 'list'
+
+    if @user
+      gists = GistList.new(@user.id, params[:page])
+
+      headers 'X-Cache-Hit' => gists.from_redis
+
+      if gists.list.empty?
+        status 404
+        return erb :invalid_gist, :locals => { :gist_id => false }
+      end
+
+      respond_to do |wants|
+        wants.html { erb :list, :locals => {:gists => gists} }
+        wants.json { gists.listify.to_json }
+        wants.xml { erb :list, :locals => {:gists => gists} }
+      end
+
+    else
+      redirect '/'
+    end
+  end
+
+
+  get %r{(?:/)?([\w-]+)?/([\w]+)$} do
+    @action = 'view'
+    id = params[:captures].last
+    valid = true
+
+    @gist = Gist.new(id)
+
+    if ! @gist.content
+      @gist = false
+
       status 404
-      return erb :invalid_gist, :locals => { :gist_id => false }
+      return erb :invalid_gist, :locals => { :gist_id => id }
+    end
+
+    @user = User.new(params[:captures].first) unless @user
+
+    if request.url == @gist.roughdraft_url
+      headers 'X-Cache-Hit' => @gist.from_redis
+
+      erb :gist
+    else
+      redirect to(@gist.roughdraft_url)
+    end
+  end
+
+
+  get %r{(?:/)?([\w-]+)?/([\w]+)/edit$} do
+    @action = 'edit'
+    id = params[:captures].last
+
+    @gist = Gist.new(id)
+
+    if ! @gist.content
+      @gist = false
+
+      status 404
+      return erb :invalid_gist, :locals => { :gist_id => id }
+    end
+
+    @user = User.new(params[:captures].first) unless @user
+
+    if request.url == "#{@gist.roughdraft_url}/edit" && @gist.belongs_to?(session[:github_id])
+      headers 'X-Cache-Hit' => @gist.from_redis
+
+      erb :'edit-gist'
+    else
+      redirect to(@gist.roughdraft_url)
+    end
+  end
+
+
+  post %r{(?:/)?([\w-]+)?/([\w]+)/update$} do
+    @action = 'update'
+    id = params[:captures].last
+
+    @gist = Gist.new(id)
+
+    foo = @gist.update(params[:title], params[:contents], session)
+
+    respond_to do |wants|
+      wants.json { foo.to_json }
+    end
+  end
+
+
+  delete %r{(?:/)?([\w-]+)?/([\w]+)/delete$} do
+    @action = 'delete'
+    id = params[:captures].last
+
+    delete = Gist.new(id).delete(session)
+    GistList.new(session[:github_id]).purge
+
+    respond_to do |wants|
+      wants.json { id }
+    end
+  end
+
+
+  get '/new' do
+    @action = 'new'
+
+    erb :'new-gist'
+  end
+
+  post '/preview' do
+    @action = 'preview'
+
+    hash = Hash.new
+    hash['description'] = params[:title]
+    hash['files'] = Array.new
+
+    params[:contents].each do |key, value|
+      html = Hashie::Mash.new
+
+      ext = File.extname(key)
+
+      if ext.match(/\.(textile)/)
+        html.language = 'Textile'
+      elsif ext.match(/\.(haml)/)
+        html.language = 'Haml'
+      else
+        html.language = 'Markdown'
+      end
+
+      html.content = value["content"]
+
+      hash['files'] << Roughdraft.gist_pipeline(html, params[:contents])
+    end
+
+    hash.to_json.to_s
+  end
+
+
+  post '/create' do
+    params[:title]
+    params[:contents]
+
+    data = Chairman.session(session[:github_token]).gists.create(description: params[:title], public: true, files: params[:contents])
+
+    respond_to do |wants|
+      wants.json { "/#{data.id.to_s}/edit".to_json }
+    end
+  end
+
+
+  get %r{(?:/)?([\w-]+)?/([\w]+)/comments$} do
+    @action = 'comments'
+    id = params[:captures].last
+
+    comments = GistComments.new(id)
+
+    if ! comments
+      status 404
+      return ''
     end
 
     respond_to do |wants|
-      wants.html { erb :list, :locals => {:gists => gists} }    # => sets content_type to text/html
-      wants.json { gists.listify.to_json } # => sets content_type to application/json
-      # wants.js { erb :comment }       # => views/comment.js.erb, also sets content_type to application/javascript
-      wants.xml { erb :list, :locals => {:gists => gists} }    # also sets content_type to application/xml
-      #
+      wants.json { comments.list.to_json }
     end
-
-  else
-    redirect '/'
   end
-end
-
-
-get %r{(?:/)?([\w-]+)?/([\w]+)$} do
-  @action = 'view'
-  id = params[:captures].last
-  valid = true
-
-  @gist = Gist.new(id)
-
-  if ! @gist.content
-    @gist = false
-
-    status 404
-    return erb :invalid_gist, :locals => { :gist_id => id }
-  end
-
-  @user = User.new(params[:captures].first) unless @user
-
-  if request.url == @gist.roughdraft_url
-    headers 'X-Cache-Hit' => @gist.from_redis
-
-    erb :gist
-  else
-    redirect to(@gist.roughdraft_url)
-  end
-end
-
-
-get %r{(?:/)?([\w-]+)?/([\w]+)/edit$} do
-  @action = 'edit'
-  id = params[:captures].last
-
-  @gist = Gist.new(id)
-
-  if ! @gist.content
-    @gist = false
-
-    status 404
-    return erb :invalid_gist, :locals => { :gist_id => id }
-  end
-
-  @user = User.new(params[:captures].first) unless @user
-
-  if request.url == "#{@gist.roughdraft_url}/edit" && @gist.belongs_to?(session[:github_id])
-    headers 'X-Cache-Hit' => @gist.from_redis
-
-    erb :'edit-gist'
-  else
-    redirect to(@gist.roughdraft_url)
-  end
-end
-
-
-post %r{(?:/)?([\w-]+)?/([\w]+)/update$} do
-  @action = 'update'
-  id = params[:captures].last
-
-  @gist = Gist.new(id)
-
-  foo = @gist.update(params[:title], params[:contents], session)
-
-  respond_to do |wants|
-    # wants.html { erb :list, :locals => {:gists => gists} }    # => views/comment.html.haml, also sets content_type to text/html
-    wants.json { foo.to_json } # => sets content_type to application/json
-    # wants.js { erb :comment }       # => views/comment.js.erb, also sets content_type to application/javascript
-  end
-end
-
-
-delete %r{(?:/)?([\w-]+)?/([\w]+)/delete$} do
-  @action = 'delete'
-  id = params[:captures].last
-
-  delete = Gist.new(id).delete(session)
-  GistList.new(session[:github_id]).purge
-
-
-  respond_to do |wants|
-    # wants.html { erb :list, :locals => {:gists => gists} }    # => views/comment.html.haml, also sets content_type to text/html
-    wants.json { id } # => sets content_type to application/json
-    # wants.js { erb :comment }       # => views/comment.js.erb, also sets content_type to application/javascript
-  end
-end
-
-
-get '/new' do
-  @action = 'new'
-
-  erb :'new-gist'
-end
-
-post '/preview' do
-  @action = 'preview'
-
-  hash = Hash.new
-  hash['description'] = params[:title]
-  hash['files'] = Array.new
-
-  params[:contents].each do |key, value|
-    html = Hashie::Mash.new
-
-    ext = File.extname(key)
-
-    if ext.match(/\.(textile)/)
-      html.language = 'Textile'
-    elsif ext.match(/\.(haml)/)
-      html.language = 'Haml'
-    else
-      html.language = 'Markdown'
-    end
-
-    html.content = value["content"]
-
-    hash['files'] << Roughdraft.gist_pipeline(html, params[:contents])
-  end
-
-  hash.to_json.to_s
-end
-
-
-post '/create' do
-  params[:title]
-  params[:contents]
-
-  data = Chairman.session(session[:github_token]).gists.create(description: params[:title], public: true, files: params[:contents])
-
-  respond_to do |wants|
-    # wants.html { erb :list, :locals => {:gists => gists} }    # => views/comment.html.haml, also sets content_type to text/html
-    wants.json { "/#{data.id.to_s}/edit".to_json } # => sets content_type to application/json
-    # wants.js { erb :comment }       # => views/comment.js.erb, also sets content_type to application/javascript
-  end
-end
-
-
-get %r{(?:/)?([\w-]+)?/([\w]+)/comments$} do
-  @action = 'comments'
-  id = params[:captures].last
-
-  comments = GistComments.new(id)
-
-  if ! comments
-    status 404
-    return ''
-  end
-
-
-  respond_to do |wants|
-    # wants.html { erb :list, :locals => {:gists => gists} }    # => views/comment.html.haml, also sets content_type to text/html
-    wants.json { comments.list.to_json } # => sets content_type to application/json
-    # wants.js { erb :comment }       # => views/comment.js.erb, also sets content_type to application/javascript
-  end
-end
-
 
 
   use Chairman::Routes
