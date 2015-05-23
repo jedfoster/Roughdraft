d257f20297d651168837046d8c04c1c9cbe7f9ec
Jed Foster <jed@jedfoster.com>
2015-05-23 15:17:27 -0700

    Update bookmarklet, remove jQuery dependency

--- client/js/bookmarklet.js
+++ client/js/bookmarklet.js
@@ -1,38 +1,28 @@
-(function(e, a, g, h, f, c, b, d) {
-	if (! (f = e.jQuery) || g > f.fn.jquery || h(f)) {
-		c = a.createElement('script');
-		c.type = 'text/javascript';
-		c.src = 'http://ajax.googleapis.com/ajax/libs/jquery/' + g + '/jquery.min.js';
-		c.onload = c.onreadystatechange = function() {
-			if (!b && (!(d = this.readyState) || d == 'loaded' || d == 'complete')) {
-				h((f = e.jQuery).noConflict(1), b = 1);
-				f(c).remove()
-			}
-		};
-		a.documentElement.childNodes[0].appendChild(c)
-	}
-})(window, document, '1.4.2', function($, L) {
-	var gist_re = /^https:\/\/gist\.github\.com\/([\w-]+)*?\/*?(\w+)$/i,
-		rel_re = /^\/?(\d+)$/,
-		on_gist = gist_re.test(location.href);
-	if (on_gist) {
-		i = location.pathname.split(/\/([\w-]+)\/(\w+)/);
-		location.href = 'http://' + i[1] + '.roughdraft.io/' + i[2];
-	} else {
-		$('a').each(function() {
-			var b = $(this).attr('href') || '',
-				a = b.match(gist_re);
+(function() {
+  var gist_regex = /^https:\/\/gist\.github\.com\/([\w-]+)\/(\w+)*$/i,
+      rel_regex = /^\/?(\d+)$/,
+      on_gist = gist_regex.test(location.href);
 
-			if (on_gist && !(a && a[2])) {
-				a = b.match(rel_re)
-			}
-			if (a && a[2]) {
-			  if(typeof(a[1]) == 'undefined') {
-			    a[1] = 'www';
-			  }
+  if(on_gist) {
+    i = location.pathname.split(/\/([\w-]+)\/(\w+)/);
+    location.href = 'http://' + i[1] + '.roughdraft.io/' + i[2];
+  } 
 
-				$(this).after(' <a href=&quot;http://' + a[1] + '.roughdraft.io/' + a[2] + '&quot;>[roughdraft.io]</a>')
-			}
-		});
-	}
-});
+  else {
+    Array.prototype.forEach.call(document.querySelectorAll('a'), function(el){
+      var b = el.getAttribute('href') || '',
+          a = b.match(gist_regex);
+
+      if (on_gist && !(a && a[2])) {
+        a = b.match(rel_regex)
+      }
+      if (a && a[2]) {
+        if(typeof(a[1]) == 'undefined') {
+          a[1] = 'www';
+        }
+
+        el.insertAdjacentHTML('afterend', ' <a href=&quot;http://' + a[1] + '.roughdraft.io/' + a[2] + '&quot;>[roughdraft.io]</a>');
+      }
+    });
+  }
+})();
